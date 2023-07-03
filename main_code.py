890f7d3373e9b452a00ed2bb4a446c1a356c6ee2
Gabrijel <g.gordic@outlook.com>
2023-07-03 15:51:03 +0200

    Gotovo ali ne u zeljenom formatu

--- main_code.py
+++ main_code.py
@@ -1,49 +1,62 @@
 import serial
+import datetime
 import time
 import csv
+import customtkinter as ctk
+import tkinter as tk
+import keyboard
 
-
-
-
-
-# Open the serial connection
-ser = serial.Serial('COM9', 9600)  # Replace 'COM1' with the appropriate port and '9600' with the baud rate
-command = "\W"  # Replace with the command/query to request weight from the scale
-
-
-readings_list = []
-
-
-replacements = [("ST",""),(",",""),("GS",""),("UG","")]
-
-counter = 0
-
-while counter != 10:
-    
-# Send command to request weight
-    
-    command_bytes = command.encode()
-    ser.write(command_bytes)
-    time.sleep(0.6)
-
-# Read response from the scale
-    response = str(ser.readline().decode().strip())
-    response = response.replace('ST,GS',"").replace(" ","").replace("US,GS","")
-    print("Scale response:", response)
-    readings_list.append(response)
-    
-    counter += 1
-
-# Close the serial connection
-ser.close()
-
-csv_file_path = "readings.csv"
-
-with open(csv_file_path, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(["Reading"])  # Write header row
-    for reading in readings_list:
-        writer.writerow([reading])
+"""
+class Window(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("800x600+325+50")
+        self.title("DOK-ING SCALE")
         
-print(readings_list)
 
+if __name__ == "__main__":
+    window = Window()
+    window.mainloop()
+"""
+"""
+def stop_reading_s_key():
+    default_bool = False
+    
+    while True:
+        if keyboard.ispressed("s"):
+            default_bool = True
+"""            
+            
+def current_time():
+    time_this_moment = datetime.datetime.now()
+    time_this_moment_modified = time_this_moment.strftime("%m/%d/%Y, %H:%M:%S")
+    return time_this_moment_modified
+
+def scale_reading():
+    # Open the serial connection
+    ser = serial.Serial('COM9', 9600)  # appropriate port and '9600' baud rate
+    command = "\W"  # command/query to request weight from the scale
+    weight_list = []
+    counter = 0
+    while counter != 15:
+    # Send command to request weight
+        command_bytes = command.encode()
+        ser.write(command_bytes)
+        time.sleep(0.6)
+    # Read response from the scale
+        response = str(ser.readline().decode().strip())
+        response = response.replace('ST,GS',"").replace(" ","").replace("US,GS","").replace("kg","")
+        print("Scale response:", response)
+        weight_list.append([response,current_time()])
+        counter += 1
+    # Close the serial connection
+    ser.close()
+    csv_file_path = "readings.csv"
+
+    with open(csv_file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["WEIGHT"])  # Write header row
+        for weight_reading in weight_list:
+            writer.writerow([weight_reading])
+    
+scale_reading()
